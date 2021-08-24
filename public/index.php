93667843d5e576bfa57290f3c9a505ec548d5b2a
Maulana Yusuf <66405774+maulana2468@users.noreply.github.com>
2021-08-24 18:00:35 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -31,8 +31,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-// require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/bwa-course/bootstrap/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -45,8 +44,7 @@ require __DIR__.'/bwa-course/bootstrap/autoload.php';
 |
 */
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
-$app = require_once __DIR__.'/bwa-course/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 
 $kernel = $app->make(Kernel::class);
